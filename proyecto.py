d580070e47e7a8eeb88ba04a720cd9d9872d1f9e
JAPerea <j.a.perea.garcia@gmail.com>
2013-03-05 10:57:09 +0100

    Proyecto

--- proyecto.py
+++ proyecto.py
@@ -1,15 +1,18 @@
 from amazonproduct import API
 AWS_KEY = 'AKIAJHX3JO2RPYRT6BGA'
 SECRET_KEY = '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'
+ASSOCIATE_TAG = 'proyeamazo-21'
+params = { 'ResponseGroup' : 'Medium', }
+api = API(AWS_KEY, SECRET_KEY, 'uk', ASSOCIATE_TAG)
+for page in api.item_search("All", Keywords="linux", **params):
+    for product in page.Items.Item:
+        print product
 
-api = API(AWS_KEY, SECRET_KEY, 'uk', AssociateTag='proyeamazo-21')
-node = api.item_search('Books', Publisher='Galileo Press')
+#total_results = node.Items.TotalResults.pyval
+#total_pages = node.Items.TotalPages.pyval
 
-total_results = node.Items.TotalResults.pyval
-total_pages = node.Items.TotalPages.pyval
-
-for book in node.Items.Item:
-   print '%s: "%s"' % (book.ItemAttributes.Author,
-                        book.ItemAttributes.Title)
+#for book in node.Items.Item:
+#   print '%s: "%s"' % (book.ItemAttributes.Author,
+#                        book.ItemAttributes.Title)
 
 
