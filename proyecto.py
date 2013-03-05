5ab51723afb47278bdf10673151a8b368f7557f5
JAPerea <j.a.perea.garcia@gmail.com>
2013-03-05 14:30:05 +0100

    proyecto

--- proyecto.py
+++ proyecto.py
@@ -1,18 +1,9 @@
-from amazonproduct import API
+import bottlenose
 AWS_KEY = 'AKIAJHX3JO2RPYRT6BGA'
 SECRET_KEY = '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'
 ASSOCIATE_TAG = 'proyeamazo-21'
-params = { 'ResponseGroup' : 'Medium', }
-api = API(AWS_KEY, SECRET_KEY, 'uk', ASSOCIATE_TAG)
-for page in api.item_search("All", Keywords="linux", **params):
-    for product in page.Items.Item:
-        print product
 
-#total_results = node.Items.TotalResults.pyval
-#total_pages = node.Items.TotalPages.pyval
+amazon = bottlenose.Amazon(AWS_KEY, SECRET_KEY, ASSOCIATE_TAG)
+response = amazon.ItemSearch(Keywords="Kindle 3G", SearchIndex="All")
 
-#for book in node.Items.Item:
-#   print '%s: "%s"' % (book.ItemAttributes.Author,
-#                        book.ItemAttributes.Title)
-
-
+print response
