6752f46858ac3ea46e11622547a6a79eef92a500
JAPerea <j.a.perea.garcia@gmail.com>
2013-03-05 10:46:12 +0100

    Proyecto

--- proyecto.py
+++ proyecto.py
@@ -2,13 +2,13 @@ from amazonproduct import API
 AWS_KEY = 'AKIAJHX3JO2RPYRT6BGA'
 SECRET_KEY = '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'
 
-api = API(AWS_KEY, SECRET_KEY, 'uk')
-node = api.item_search('Books')
+api = API(AWS_KEY, SECRET_KEY, 'uk', AssociateTag='proyeamazo-21')
+node = api.item_search('Books', Publisher='Galileo Press')
 
 total_results = node.Items.TotalResults.pyval
 total_pages = node.Items.TotalPages.pyval
 
-for books in node.Items.Item:
+for book in node.Items.Item:
    print '%s: "%s"' % (book.ItemAttributes.Author,
                         book.ItemAttributes.Title)
 
