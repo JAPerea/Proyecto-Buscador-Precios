fb7c50c981875f843d2dc21f808cace47cb700de
JAPerea <j.a.perea.garcia@gmail.com>
2013-03-10 19:54:06 +0100

    Proyecto

--- proyecto.py
+++ proyecto.py
@@ -6,21 +6,26 @@ AWS_KEY = 'AKIAJHX3JO2RPYRT6BGA'
 SECRET_KEY = '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'
 ASSOCIATE_TAG = 'proyeamazo-21'
 
-r = requests.get('http://ecs.amazonaws.com/onca/xml?AWSAccessKeyId=AKIAJHX3JO2RPYRT6BGA&AssociateTag=proyeamazo-21&Keywords=Fallout%20New%20Vegas&Operation=ItemSearch&ResponseGroup=Offers%2C%20ItemAttributes&SearchIndex=All&Service=AWSECommerceService&Timestamp=2013-03-06T09%3A04%3A37.000Z&Version=2011-08-01&Signature=Ez%2Flw6I7LOQxh3qlL83TeqD3cNtr0rZF8FI9ucCmgg4%3D')
-url= r.text
+print "Content-Type: text/html\n"
+form = cgi.FieldStorage()
+nombre = form.getvalue("juego")
 
-print url
-xml = etree.fromstring(url)
+#datos= {'%s' %nombre}
+#r = requests.get('http://ecs.amazonaws.com/onca/xml?AWSAccessKeyId=AKIAJHX3JO2RPYRT6BGA&AssociateTag=proyeamazo-21&Operation=ItemSearch&ResponseGroup=Offers%2C%20ItemAttributes&SearchIndex=VideoGames&Service=AWSECommerceService&Timestamp=2013-03-07T11%3A45%3A40.000Z&Version=2011-08-01&Signature=G1Fx1wZVcXIhV1dxB7wg%2F5TrXaAhm2MFkLEDqE9j80c%3D&Keywords=', params=datos)
+#url= r.text
 
-nombre = xml.xpath("/ns:ItemSearchResponse/ns:Items/ns:Item/ns:ItemAttributes/ns:Title/text()")
-plataforma = xml.xpath("/ItemSearchResponse/Items/Item/ItemAttributes/Platform/text()")
-menos_nuevo = xml.xpath("/ItemSearchResponse/Items/Item/OfferSummary/LowestNewPrice/FormattedPrice/text()")
-menos_usado = xml.xpath("/ItemSearchResponse/Items/Item/OfferSummary/LowestUsedPrice/FormattedPrice/text()")
-
-
-print nombre
+#xml = etree.fromstring(url)
+#xml = etree.parse("xml")
 
 
+#nombre = xml.xpath("/ItemSearchResponse/Items/Item/ItemAttributes/Title/text()")
+#plataforma = xml.xpath("/ItemSearchResponse/Items/Item/ItemAttributes/Platform/text()")
+#menos_nuevo = xml.xpath("/ItemSearchResponse/Items/Item/OfferSummary/LowestNewPrice/FormattedPrice/text()")
+#menos_usado = xml.xpath("/ItemSearchResponse/Items/Item/OfferSummary/LowestUsedPrice/FormattedPrice/text()")
 
-#http://ecs.amazonaws.com/onca/xml?AWSAccessKeyId=AKIAJHX3JO2RPYRT6BGA&AssociateTag=proyeamazo-21&Keywords=fallout%20new%20vegas&Operation=ItemSearch&ResponseGroup=Offers%2C%20ItemAttributes&SearchIndex=All&Service=AWSECommerceService&Timestamp=2013-03-06T08%3A31%3A01.000Z&Version=2011-08-01&Signature=oU8HJl0hJCEz8BsdlCp%2FuIGDYDpAmDCIUQ9PLLTFp3E%3D
+#print nombre
+
+
+
+#http://ecs.amazonaws.com/onca/xml?AWSAccessKeyId=AKIAJHX3JO2RPYRT6BGA&AssociateTag=proyeamazo-21&Keywords=Fallout%20New%20Vegas&Operation=ItemSearch&ResponseGroup=Offers%2C%20ItemAttributes&SearchIndex=All&Service=AWSECommerceService&Timestamp=2013-03-06T09%3A04%3A37.000Z&Version=2011-08-01&Signature=Ez%2Flw6I7LOQxh3qlL83TeqD3cNtr0rZF8FI9ucCmgg4%3D
 
