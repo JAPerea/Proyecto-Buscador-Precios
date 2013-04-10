478905a960a4c43a8460288a588a384e86e5b263
Jose Alejandro Perea <j.a.perea.garcia@gmail.com>
2013-04-10 09:32:10 +0200

    Proyecto

--- proyecto.py
+++ proyecto.py
@@ -11,21 +11,25 @@ form = cgi.FieldStorage()
 nombre = form.getvalue("juego")
 
 datos= {'AWSAccessKeyID': 'AKIAJHX3JO2RPYRT6BGA', 'AssociateTag': 'proyeamazo-21', 'Operation': 'ItemSearch', 'ResponseGroup': 'Offers%2C%20ItemAttributes', 'SearchIndex': 'VideoGames', 'Service': 'AWSECommerceService', 'Timestamp': '', 'Version': '2011-08-01', 'Signature': '', 'Keywords': '%s' %nombre}
-busqeda = requests.get('http://ecs.amazonaws.com/onca/xml?', params=datos)
-url= busqeda.text
-
-#xml = etree.fromstring(url)
-#xml = etree.parse("xml")
+busqueda = requests.get('http://ecs.amazonaws.com/onca/xml?', params=datos)
+url= busqueda.text
 
 
-#nombre = xml.xpath("/ItemSearchResponse/Items/Item/ItemAttributes/Title/text()")
-#plataforma = xml.xpath("/ItemSearchResponse/Items/Item/ItemAttributes/Platform/text()")
-#menos_nuevo = xml.xpath("/ItemSearchResponse/Items/Item/OfferSummary/LowestNewPrice/FormattedPrice/text()")
-#menos_usado = xml.xpath("/ItemSearchResponse/Items/Item/OfferSummary/LowestUsedPrice/FormattedPrice/text()")
+xml = etree.parse("xml")
+ns = {"ns": "http://webservices.amazon.com/AWSECommerceService/2011-08-01"}
+#tree = etree.fromstring(xml)
+for node in xml.xpath('/ItemSearchResponse/Items/Item/ItemAttributes/Title/text()', namespaces=ns):
+#for node in xml.xpath('/ns:ItemSearchResponse/Items/Item/ItemAttributes/Title/text()', namespaces=ns):
+print node
+nombre = xml.xpath("/ItemSearchResponse/Items/Item/ItemAttributes/Title/text()")
+plataforma = xml.xpath("/ItemSearchResponse/Items/Item/ItemAttributes/Platform/text()")
+menos_nuevo = xml.xpath("/ItemSearchResponse/Items/Item/OfferSummary/LowestNewPrice/FormattedPrice/text()")
+menos_usado = xml.xpath("/ItemSearchResponse/Items/Item/OfferSummary/LowestUsedPrice/FormattedPrice/text()")
 
-#print nombre
-
+print nombre
 #http://ecs.amazonaws.com/onca/xml?AWSAccessKeyId=AKIAJHX3JO2RPYRT6BGA&AssociateTag=proyeamazo-21&Operation=ItemSearch&ResponseGroup=Offers%2C%20ItemAttributes&SearchIndex=VideoGames&Service=AWSECommerceService&Timestamp=2013-03-07T11%3A45%3A40.000Z&Version=2011-08-01&Signature=G1Fx1wZVcXIhV1dxB7wg%2F5TrXaAhm2MFkLEDqE9j80c%3D&Keywords=
+
+
 
 
 
