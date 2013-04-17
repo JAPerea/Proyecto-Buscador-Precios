e00c61ec9c3d9d77b14b160112bc976358562ba9
JAPerea <j.a.perea.garcia@gmail.com>
2013-04-17 09:56:39 +0200

    proyecto

--- proyecto.py
+++ proyecto.py
@@ -15,16 +15,15 @@ busqueda = requests.get('http://ecs.amazonaws.com/onca/xml?', params=datos)
 url= busqueda.text
 
 
-xml = etree.parse("xml")
+xml = etree.parse("xml2")
 ns = {"ns": "http://webservices.amazon.com/AWSECommerceService/2011-08-01"}
 #tree = etree.fromstring(xml)
-for node in xml.xpath('/ItemSearchResponse/Items/Item/ItemAttributes/Title/text()', namespaces=ns):
-#for node in xml.xpath('/ns:ItemSearchResponse/Items/Item/ItemAttributes/Title/text()', namespaces=ns):
-print node
-nombre = xml.xpath("/ItemSearchResponse/Items/Item/ItemAttributes/Title/text()")
-plataforma = xml.xpath("/ItemSearchResponse/Items/Item/ItemAttributes/Platform/text()")
-menos_nuevo = xml.xpath("/ItemSearchResponse/Items/Item/OfferSummary/LowestNewPrice/FormattedPrice/text()")
-menos_usado = xml.xpath("/ItemSearchResponse/Items/Item/OfferSummary/LowestUsedPrice/FormattedPrice/text()")
+#for node in xml.xpath('/ns:ItemSearchResponse/ns:Items/ns:Item/ns:ItemAttributes', namespaces=ns):
+#print node
+nombre = xml.xpath("/ns:ItemSearchResponse/ns:Items/ns:Item/ns:ItemAttributes/ns:Title/text()", namespaces=ns)
+plataforma = xml.xpath("/ns:ItemSearchResponse/ns:Items/ns:Item/ns:ItemAttributes/ns:Platform/text()", namespaces=ns)
+menos_nuevo = xml.xpath("/ns:ItemSearchResponse/ns:Items/ns:Item/ns:OfferSummary/ns:LowestNewPrice/ns:FormattedPrice/text()", namespaces=ns)
+menos_usado = xml.xpath("/ns:ItemSearchResponse/ns:Items/ns:Item/ns:OfferSummary/ns:LowestUsedPrice/ns:FormattedPrice/text()", namespaces=ns)
 
 print nombre
 #http://ecs.amazonaws.com/onca/xml?AWSAccessKeyId=AKIAJHX3JO2RPYRT6BGA&AssociateTag=proyeamazo-21&Operation=ItemSearch&ResponseGroup=Offers%2C%20ItemAttributes&SearchIndex=VideoGames&Service=AWSECommerceService&Timestamp=2013-03-07T11%3A45%3A40.000Z&Version=2011-08-01&Signature=G1Fx1wZVcXIhV1dxB7wg%2F5TrXaAhm2MFkLEDqE9j80c%3D&Keywords=
