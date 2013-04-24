880025e2428ff8fc91bc563d4bdf29fed8e23edb
JAPerea <j.a.perea.garcia@gmail.com>
2013-04-24 08:48:35 +0200

    proyecto

--- proyecto.py
+++ proyecto.py
@@ -17,24 +17,17 @@ url= busqueda.text
 
 xml = etree.parse("xml2")
 ns = {"ns": "http://webservices.amazon.com/AWSECommerceService/2011-08-01"}
-#tree = etree.fromstring(xml)
-#for node in xml.xpath('/ns:ItemSearchResponse/ns:Items/ns:Item/ns:ItemAttributes', namespaces=ns):
-#print node
-nombre = xml.xpath("/ns:ItemSearchResponse/ns:Items/ns:Item/ns:ItemAttributes/ns:Title/text()", namespaces=ns)
-plataforma = xml.xpath("/ns:ItemSearchResponse/ns:Items/ns:Item/ns:ItemAttributes/ns:Platform/text()", namespaces=ns)
-menos_nuevo = xml.xpath("/ns:ItemSearchResponse/ns:Items/ns:Item/ns:OfferSummary/ns:LowestNewPrice/ns:FormattedPrice/text()", namespaces=ns)
-menos_usado = xml.xpath("/ns:ItemSearchResponse/ns:Items/ns:Item/ns:OfferSummary/ns:LowestUsedPrice/ns:FormattedPrice/text()", namespaces=ns)
 
-productos= len(xml.xpath('/ns:ItemSearchResponse/ns:Items/ns:Item/ns:ItemAttributes', namespaces=ns))
+productos= len(xml.xpath('/ns:ItemSearchResponse/ns:Items/ns:Item', namespaces=ns))
 for i in xrange(productos):
-    lista= xml.xpath('/ns:ItemSearchResponse/ns:Items/ns:Item/ns:ItemAttributes', namespaces=ns)[i]
-    print lista.xpath('ns:Title/text()', namespaces=ns)
-    print lista.xpath('ns:Platform/text()', namespaces=ns)
+    	lista= xml.xpath('/ns:ItemSearchResponse/ns:Items/ns:Item', namespaces=ns)[i]
+    	print lista.xpath('ns:ItemAttributes/ns:Title/text()', namespaces=ns)
+    	print lista.xpath('ns:ItemAttributes/ns:Platform/text()', namespaces=ns)
+	print lista.xpath('ns:OfferSummary/ns:LowestNewPrice/ns:FormattedPrice/text()', namespaces=ns)
+	print lista.xpath('ns:OfferSummary/ns:LowestUsedPrice/ns:FormattedPrice/text()', namespaces=ns)
 
 
 	
-
-print nombre
 #http://ecs.amazonaws.com/onca/xml?AWSAccessKeyId=AKIAJHX3JO2RPYRT6BGA&AssociateTag=proyeamazo-21&Operation=ItemSearch&ResponseGroup=Offers%2C%20ItemAttributes&SearchIndex=VideoGames&Service=AWSECommerceService&Timestamp=2013-03-07T11%3A45%3A40.000Z&Version=2011-08-01&Signature=G1Fx1wZVcXIhV1dxB7wg%2F5TrXaAhm2MFkLEDqE9j80c%3D&Keywords=
 
 
